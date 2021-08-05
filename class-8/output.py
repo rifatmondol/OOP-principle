4a72430d891d23f293c94002d37f223a9bc5da3c
rifat mondol <rifatmondol791@gmail.com>
2021-08-06 02:32:29 +0600

    Update output.py

--- class-8/output.py
+++ class-8/output.py
@@ -5,6 +5,6 @@
 
 from account import Account
 
-acc = Account(" Rashed Islam", "+8801754657643", "abc@gmail.com", "Dhaka", 3000, 5000,  1500)
+acc = Account(" Rashed Islam", "+8801754657643", "abc@gmail.com", "Dhaka", 5000, 10000,  3500)
 print(acc)
 print(" Deposite:", acc.get_deposite(), "\n", "Total Balance:", acc.get_balance(), "\n", "After Withdraw, total balance:", acc.get_withdraw())
